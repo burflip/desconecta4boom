67117d1470a5d9c055adc3e3e2999a66e318ec98
Valentin <burflip@gmail.com>
2016-05-24 11:26:56 +0200

    Commit inicial con la práctica y sugerencias del profesor

---
+++ player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "environment.h"
+
+class Player{
+    public:
+      Player(int jug);
+      Environment::ActionType Think();
+      void Perceive(const Environment &env);
+      double Practicar(const Environment &T, int jugador, int prof, int &accion);
+    private:
+      int jugador_;
+      Environment actual_;
+};
+#endif
